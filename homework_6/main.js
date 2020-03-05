16bbb9cd8b3981004881d26d0211098c0b0d9429
EmmetMack <emack@andrew.cmu.edu>
2020-03-05 18:59:08 -0500

    Updating image on product details page when changing the size and color

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,3 +1,79 @@
+function updateProductImage() {
+
+	var size = document.getElementById("size").value;
+	var color = document.getElementById("color").value;
+	var harnessImg = document.getElementById("harness");
+
+	if (size === "Tiny" && color === "Strawberry") {
+
+		harnessImg.setAttribute("src", "images/red-dog-harness-tiny.jpg");
+
+	} else if (size === "Tiny" && color === "Crazyberry") {
+
+		harnessImg.setAttribute("src", "images/crazy-berry-harness-tiny.jpg");
+
+	} else if (size === "Tiny" && color === "Blackberry") {
+
+		harnessImg.setAttribute("src", "images/blackberry-dog-harness-tiny.jpg");
+
+	} else if (size === "Tiny" && color === "FireOrange") {
+
+		harnessImg.setAttribute("src", "images/fire-orange-harness-tiny.jpg");
+
+	} else if (size === "Small" && color === "Strawberry") {
+
+		harnessImg.setAttribute("src", "images/red-dog-harness-small.jpg");
+
+	} else if (size === "Small" && color === "Crazyberry") {
+
+		harnessImg.setAttribute("src", "images/crazy-berry-harness-small.jpg");
+
+	} else if (size === "Small" && color === "Blackberry") {
+
+		harnessImg.setAttribute("src", "images/blackberry-dog-harness-small.jpg");
+
+	} else if (size === "Small" && color === "FireOrange") {
+
+		harnessImg.setAttribute("src", "images/fire-orange-harness-small.jpg");
+
+	} else if (size === "Medium" && color === "Strawberry") {
+
+		harnessImg.setAttribute("src", "images/red-dog-harness-medium.jpg");
+
+	} else if (size === "Medium" && color === "Crazyberry") {
+
+		harnessImg.setAttribute("src", "images/crazy-berry-harness-medium.jpg");
+
+	} else if (size === "Medium" && color === "Blackberry") {
+
+		harnessImg.setAttribute("src", "images/blackberry-dog-harness-medium.jpg");
+
+	} else if (size === "Medium" && color === "FireOrange") {
+
+		harnessImg.setAttribute("src", "images/fire-orange-harness-medium.jpg");
+
+	} else if (size === "Large" && color === "Strawberry") {
+
+		harnessImg.setAttribute("src", "images/red-dog-harness-large.jpg");
+
+	} else if (size === "Large" && color === "Crazyberry") {
+
+		harnessImg.setAttribute("src", "images/crazy-berry-harness-large.jpg");
+
+	} else if (size === "Large" && color === "Blackberry") {
+
+		harnessImg.setAttribute("src", "images/blackberry-dog-harness-large.jpg");
+
+	} else if (size === "Large" && color === "FireOrange") {
+
+		harnessImg.setAttribute("src", "images/fire-orange-harness-large.jpg");
+
+	}
+
+}
+
+
+// cart logic below
 
 function Cart(items) {
 	this.items = items;
@@ -32,8 +108,6 @@ function addToCart() {
 	document.getElementById("cart-link").innerHTML = "Cart(" + (cartTotalSize) + ")";
 
 	
-
-
 }
 
 function Item(name, color, size, quantity, price) {
