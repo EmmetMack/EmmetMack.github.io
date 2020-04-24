9579a9fcce5c83af703eb84ddff88a17da0aa825
EmmetMack <emack@andrew.cmu.edu>
2020-04-24 14:21:21 -0400

    Debugging the problems with selecting teams

--- homework_8/main.js
+++ homework_8/main.js
@@ -39,12 +39,12 @@ const SpainCountryID = "775";
 
 
 
-document.getElementById('country-sel').addEventListener('change', getTeams()); // event listner for when a team is selected
+document.getElementById('#country-sel').addEventListener('change', getTeams()); // event listner for when a team is selected
 
 //function that gets the teams for a specified league then adds the teams to the Team dropdown menu
 function getTeams() {
 
-	selectElement = document.getElementById('#country-sel'); 
+	selectElement = document.getElementById('country-sel'); 
 
 	console.log(selectElement);
                       
