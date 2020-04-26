a3cf950578f7e36975a68780c842a52959f237dd
EmmetMack <emack@andrew.cmu.edu>
2020-04-25 23:11:54 -0400

    geocoder pls

--- homework_8/main.js
+++ homework_8/main.js
@@ -19,23 +19,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: mapboxToken
 }).addTo(mymap);
 
-const maps_api_key = "AIzaSyATi23iBvfHagwQ5C_U3-qPzCfBJCTzouE";
-
-var location = "Paris, " + country_codes["France"];
-
-var request = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + maps_api_key;
-
-var geocoder = new google.maps.Geocoder();
-geocoder.geocode( {address: location, key: maps_api_key} , function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-        var location = results[0].geometry.location,
-            lat      = location.lat(),
-            lng      = location.lng();
-      console.log("Latitude: " + lat);
-      console.log("Longitude: " + lng);
-    }
-});
-
 //country names to codes for geodecoding 
 const country_codes = {
 	"Andorra": "AD",
@@ -153,8 +136,22 @@ const country_codes = {
 	"South Africa" : "ZA"
 }
 
+const maps_api_key = "AIzaSyATi23iBvfHagwQ5C_U3-qPzCfBJCTzouE";
 
+var location = "Paris, " + country_codes["France"];
 
+var request = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + maps_api_key;
+
+var geocoder = new google.maps.Geocoder();
+geocoder.geocode( {address: location, key: maps_api_key} , function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+        var location = results[0].geometry.location,
+            lat      = location.lat(),
+            lng      = location.lng();
+      console.log("Latitude: " + lat);
+      console.log("Longitude: " + lng);
+    }
+});
 
 //Football API requests
 
