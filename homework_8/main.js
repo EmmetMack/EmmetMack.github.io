52cf7b2d73ba697ec990b2073192ab53e89bcff7
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 13:56:33 -0400

    trying leaf-heat

--- homework_8/main.js
+++ homework_8/main.js
@@ -622,87 +622,23 @@ function createPlayerForLeague(teams) {
 	var myGeoJSON = createGeoJSON(leaguePlayers);
 	console.log(myGeoJSON);
 	
-	gl.getMapboxMap().addSource('soccer_countries', {
-		'type': 'geojson',
-		'data': myGeoJSON
+	L.geoJSON(myGeoJSON["features"]).addTo(mymap);
+	var soccerPoints = [];
+	myGeoJSON["features"].forEach(function(feature){
+		var cords = feature["geometry"]["coordinates"];
+		var count = feature["properties"]["count"];
+		soccerPoints.push([cords[1], cords[0], count]);
 	});
 
-	var map = gl.getMapboxMap();
-	console.log(map);
-	
-	gl.getMapboxMap().addLayer(
-		{
-		'id': 'soccer-nations',
-		'type': 'heatmap',
-		'source': 'soccer_countries',
-		'maxzoom': 9,
-		'paint': {
-		// Increase the heatmap weight based on frequency and property magnitude
-		'heatmap-weight': [
-		'interpolate',
-		['linear'],
-		['get', 'count'],
-		0,
-		0,
-		6,
-		1
-		],
-		// Increase the heatmap color weight weight by zoom level
-		// heatmap-intensity is a multiplier on top of heatmap-weight
-		'heatmap-intensity': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		0,
-		1,
-		9,
-		3
-		],
-		// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-		// Begin color ramp at 0-stop with a 0-transparancy color
-		// to create a blur-like effect.
-		'heatmap-color': [
-		'interpolate',
-		['linear'],
-		['heatmap-density'],
-		0,
-		'rgba(33,102,172,0)',
-		0.2,
-		'rgb(103,169,207)',
-		0.4,
-		'rgb(209,229,240)',
-		0.6,
-		'rgb(253,219,199)',
-		0.8,
-		'rgb(239,138,98)',
-		1,
-		'rgb(178,24,43)'
-		],
-		// Adjust the heatmap radius by zoom level
-		'heatmap-radius': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		0,
-		2,
-		9,
-		20
-		],
-		// Transition from heatmap to circle layer by zoom level
-		'heatmap-opacity': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		7,
-		1,
-		9,
-		0
-		]
-		}
-		}
-		);
 
-	//L.geoJSON(myGeoJSON["features"]).addTo(mymap);
+	 var heat = L.heatLayer(soccerPoints,{
+            radius: 20,
+            blur: 15, 
+            maxZoom: 17,
+            max: 400,
+      }).addTo(mymap);
+
+
 }
 
 
@@ -712,7 +648,6 @@ function createGeoJSON(players) {
 	console.log("createGeoJSON called");
 
 	var locationJSON = {
-						"type": "FeatureCollection",
 					    "features": []
 	};
 
