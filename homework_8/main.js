b38f2851ba1070e8704f67ffcd2cff42359173cd
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 12:33:52 -0400

    Seeing if this will add geojson to map

--- homework_8/main.js
+++ homework_8/main.js
@@ -614,64 +614,10 @@ function createPlayerForLeague(teams) {
 
 	// console.log(leaguePlayers);
 
-	var geoJSON = createGeoJSON(leaguePlayers);
-	console.log(geoJSON);
-	map.addSource('soccer_countries', {
-		'type': 'geojson',
-		'data': geoJSON
-	});
+	var myGeoJSON = createGeoJSON(leaguePlayers);
+	console.log(myGeoJSON);
 	
-	map.addLayer(
-		{
-			'id': 'soccer_points',
-			'type': 'circle',
-			'source': 'soccer_countries',
-			'minzoom': 7,
-			'paint': {
-			// Size circle radius by earthquake magnitude and zoom level
-			'circle-radius': [
-			'interpolate',
-			['linear'],
-			['zoom'],
-			7,
-			['interpolate', ['linear'], ['get', 'count'], 1, 1, 6, 4],
-			16,
-			['interpolate', ['linear'], ['get', 'count'], 1, 5, 6, 50]
-			],
-			// Color circle by earthquake magnitude
-			'circle-color': [
-			'interpolate',
-			['linear'],
-			['get', 'count'],
-			1,
-			'rgba(33,102,172,0)',
-			2,
-			'rgb(103,169,207)',
-			3,
-			'rgb(209,229,240)',
-			4,
-			'rgb(253,219,199)',
-			5,
-			'rgb(239,138,98)',
-			6,
-			'rgb(178,24,43)'
-			],
-			'circle-stroke-color': 'white',
-			'circle-stroke-width': 1,
-			// Transition from heatmap to circle layer by zoom level
-			'circle-opacity': [
-			'interpolate',
-			['linear'],
-			['zoom'],
-			7,
-			0,
-			8,
-			1
-			]
-			}
-			},
-			'waterway-label'
-			);
+	L.geoJSON(myGeoJSON).addTo(mymap);
 }
 
 
