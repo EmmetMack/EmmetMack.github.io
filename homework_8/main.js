9a6a591d69dcf1594efb85830f1cb0a0eec1bc7f
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 15:05:47 -0400

    Messing with settings trying to get heatmap to work

--- homework_8/main.js
+++ homework_8/main.js
@@ -12,9 +12,9 @@ var mymap = L.map('mapid').setView([51.509865, -0.118092], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxToken, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 17,
     id: 'mapbox/light-v10',
-    tileSize: 256,
+    tileSize: 516,
     zoomOffset: -1,
     accessToken: mapboxToken
 }).addTo(mymap);
