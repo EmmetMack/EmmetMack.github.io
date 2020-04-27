70f5e4f5cf53dd522a5be85482751b21ed7452b1
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 12:21:33 -0400

    Trying to fix looping issue

--- homework_8/main.js
+++ homework_8/main.js
@@ -661,7 +661,7 @@ function createGeoJSON(players) {
  	console.log(countryCounts);
  	
  	Object.keys(countryCounts).forEach(function(key) {
-
+ 		console.log(key);
  		var countryJSON = {
  			"geometry": {
 
