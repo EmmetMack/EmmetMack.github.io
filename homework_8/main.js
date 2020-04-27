ef3dd35acffb193f5ccd396fc546c4ceb39473e3
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 14:04:20 -0400

    Yeet

--- homework_8/main.js
+++ homework_8/main.js
@@ -8,7 +8,7 @@ import * as LaLiga from './modules/laliga-JSON.js'
 const mapboxToken = "pk.eyJ1IjoiZW1hY2siLCJhIjoiY2s5N2JrNHduMHRlOTNwbGNraHEwaWd3MyJ9.VvKNrlGdjwUi6dUOaWDx8A"
 
 //Leaflet map stuff
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([51.505, -0.09], 2);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxToken, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
