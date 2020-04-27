516fcb5c13f4a2879b66b828de0a742e674a91f3
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 15:17:44 -0400

    Trying to get mapbox to work

--- homework_8/main.js
+++ homework_8/main.js
@@ -10,14 +10,25 @@ const mapboxToken = "pk.eyJ1IjoiZW1hY2siLCJhIjoiY2s5N2JrNHduMHRlOTNwbGNraHEwaWd3
 //Leaflet map stuff
 var mymap = L.map('mapid').setView([51.509865, -0.118092], 4);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxToken, {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 17,
-    id: 'mapbox/light-v10',
-    tileSize: 516,
-    zoomOffset: -1,
-    accessToken: mapboxToken
-}).addTo(mymap);
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxToken, {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 17,
+//     id: 'mapbox/light-v10',
+//     tileSize: 516,
+//     zoomOffset: -1,
+//     accessToken: mapboxToken
+// }).addTo(mymap);
+17
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/dark-v10',
+	center: [-120, 50],
+	zoom: 2
+});
+
+
+// Add a geojson point source.
+// Heatmap layers also work with a vector tile source.
 
 //country names to codes for geodecoding 
 const country_lat = {
@@ -617,33 +628,110 @@ function createPlayerForLeague(teams) {
 	var myGeoJSON = createGeoJSON(leaguePlayers);
 	console.log(myGeoJSON);
 	
-	// L.geoJSON(myGeoJSON["features"]).addTo(mymap);
-	var maxCount = -1;
 
-	var soccerPoints = [];
-	myGeoJSON["features"].forEach(function(feature){
-		console.log("feature: " + feature);
-		var cords = feature["geometry"]["coordinates"];
-		var count = feature["properties"]["count"];
-		var latLng = new L.latLng(cords[0], cords[1], count);
-		console.log(latLng);
-		if (count > maxCount) {
-			maxCount = count;
-		}
-		soccerPoints.push(latLng);
+	map.addSource('soccer', {
+		'type': 'geojson',
+		'data': myGeoJSON;
+		
 	});
-	console.log("soccerPoints: " + soccerPoints);
 
-	console.log("maxCount: " + maxCount);
+	map.addLayer({
 
-	var heat = L.heatLayer(soccerPoints, {
-            radius: 20,
-            blur: 15, 
-            maxZoom: 17,
-            max: maxCount,
-      }).addTo(mymap);
+		'id': 'earthquakes-heat',
+		'type': 'heatmap',
+		'source': 'earthquakes',
+		'maxzoom': 9,
+		'paint': {
+		// Increase the heatmap weight based on frequency and property magnitude
+		'heatmap-weight': [
+		'interpolate',
+		['linear'],
+		['get', 'count'],
+		0,
+		0,
+		6,
+		1
+		],
+		// Increase the heatmap color weight weight by zoom level
+		// heatmap-intensity is a multiplier on top of heatmap-weight
+		'heatmap-intensity': [
+		'interpolate',
+		['linear'],
+		['zoom'],
+		0,
+		1,
+		9,
+		3
+		],
+		// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+		// Begin color ramp at 0-stop with a 0-transparancy color
+		// to create a blur-like effect.
+		'heatmap-color': [
+		'interpolate',
+		['linear'],
+		['heatmap-density'],
+		0,
+		'rgba(33,102,172,0)',
+		0.2,
+		'rgb(103,169,207)',
+		0.4,
+		'rgb(209,229,240)',
+		0.6,
+		'rgb(253,219,199)',
+		0.8,
+		'rgb(239,138,98)',
+		1,
+		'rgb(178,24,43)'
+		],
+		// Adjust the heatmap radius by zoom level
+		'heatmap-radius': [
+		'interpolate',
+		['linear'],
+		['zoom'],
+		0,
+		2,
+		9,
+		20
+		],
+		// Transition from heatmap to circle layer by zoom level
+		'heatmap-opacity': [
+		'interpolate',
+		['linear'],
+		['zoom'],
+		7,
+		1,
+		9,
+		0
+		]
+	}
+}
+	// // L.geoJSON(myGeoJSON["features"]).addTo(mymap);
+	// var maxCount = -1;
 
-	 heat.redraw();
+	// var soccerPoints = [];
+	// myGeoJSON["features"].forEach(function(feature){
+	// 	console.log("feature: " + feature);
+	// 	var cords = feature["geometry"]["coordinates"];
+	// 	var count = feature["properties"]["count"];
+	// 	var latLng = new L.latLng(cords[0], cords[1], count);
+	// 	console.log(latLng);
+	// 	if (count > maxCount) {
+	// 		maxCount = count;
+	// 	}
+	// 	soccerPoints.push(latLng);
+	// });
+	// console.log("soccerPoints: " + soccerPoints);
+
+	// console.log("maxCount: " + maxCount);
+
+	// var heat = L.heatLayer(soccerPoints, {
+ //            radius: 20,
+ //            blur: 15, 
+ //            maxZoom: 17,
+ //            max: maxCount,
+ //      }).addTo(mymap);
+
+	//  heat.redraw();
 
 }
 
@@ -654,6 +742,7 @@ function createGeoJSON(players) {
 	console.log("createGeoJSON called");
 
 	var locationJSON = {
+					    type: "FeatureCollection", 
 					    "features": []
 	};
 
