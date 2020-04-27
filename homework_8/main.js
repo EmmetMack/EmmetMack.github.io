260fc2695b823cceba6544344bec31bec9b4043e
EmmetMack <emack@andrew.cmu.edu>
2020-04-27 15:30:51 -0400

    mapbox seems to be working

--- homework_8/main.js
+++ homework_8/main.js
@@ -638,102 +638,126 @@ function createPlayerForLeague(teams) {
 
 	map.addLayer({
 
-		'id': 'soccer-heat',
-		'type': 'heatmap',
-		'source': 'soccer',
-		'maxzoom': 9,
-		'paint': {
-		// Increase the heatmap weight based on frequency and property magnitude
-		'heatmap-weight': [
-		'interpolate',
-		['linear'],
-		['get', 'count'],
-		0,
-		0,
-		6,
-		1
-		],
-		// Increase the heatmap color weight weight by zoom level
-		// heatmap-intensity is a multiplier on top of heatmap-weight
-		'heatmap-intensity': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		0,
-		1,
-		9,
-		3
-		],
-		// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-		// Begin color ramp at 0-stop with a 0-transparancy color
-		// to create a blur-like effect.
-		'heatmap-color': [
-		'interpolate',
-		['linear'],
-		['heatmap-density'],
-		0,
-		'rgba(33,102,172,0)',
-		0.2,
-		'rgb(103,169,207)',
-		0.4,
-		'rgb(209,229,240)',
-		0.6,
-		'rgb(253,219,199)',
-		0.8,
-		'rgb(239,138,98)',
-		1,
-		'rgb(178,24,43)'
-		],
-		// Adjust the heatmap radius by zoom level
-		'heatmap-radius': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		0,
-		2,
-		9,
-		20
-		],
-		// Transition from heatmap to circle layer by zoom level
-		'heatmap-opacity': [
-		'interpolate',
-		['linear'],
-		['zoom'],
-		7,
-		1,
-		9,
-		0
-		]
+			'id': 'soccer-heat',
+			'type': 'heatmap',
+			'source': 'soccer',
+			'maxzoom': 9,
+			'paint': {
+			// Increase the heatmap weight based on frequency and property magnitude
+			'heatmap-weight': [
+			'interpolate',
+			['linear'],
+			['get', 'count'],
+			0,
+			0,
+			6,
+			1
+			],
+			// Increase the heatmap color weight weight by zoom level
+			// heatmap-intensity is a multiplier on top of heatmap-weight
+			'heatmap-intensity': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			0,
+			1,
+			9,
+			3
+			],
+			// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+			// Begin color ramp at 0-stop with a 0-transparancy color
+			// to create a blur-like effect.
+			'heatmap-color': [
+			'interpolate',
+			['linear'],
+			['heatmap-density'],
+			0,
+			'rgba(33,102,172,0)',
+			0.2,
+			'rgb(103,169,207)',
+			0.4,
+			'rgb(209,229,240)',
+			0.6,
+			'rgb(253,219,199)',
+			0.8,
+			'rgb(239,138,98)',
+			1,
+			'rgb(178,24,43)'
+			],
+			// Adjust the heatmap radius by zoom level
+			'heatmap-radius': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			0,
+			2,
+			9,
+			20
+			],
+			// Transition from heatmap to circle layer by zoom level
+			'heatmap-opacity': [
+			'interpolate',
+			['linear'],
+			['zoom'],
+			7,
+			1,
+			9,
+			0
+			]
+		}
+	});
+
+	map.addLayer(
+	{
+	'id': 'soocer-point',
+	'type': 'circle',
+	'source': 'soccer',
+	'minzoom': 7,
+	'paint': {
+	// Size circle radius by earthquake magnitude and zoom level
+	'circle-radius': [
+	'interpolate',
+	['linear'],
+	['zoom'],
+	7,
+	['interpolate', ['linear'], ['get', 'count'], 1, 1, 6, 4],
+	16,
+	['interpolate', ['linear'], ['get', 'count'], 1, 5, 6, 50]
+	],
+	// Color circle by earthquake magnitude
+	'circle-color': [
+	'interpolate',
+	['linear'],
+	['get', 'count'],
+	1,
+	'rgba(33,102,172,0)',
+	2,
+	'rgb(103,169,207)',
+	3,
+	'rgb(209,229,240)',
+	4,
+	'rgb(253,219,199)',
+	5,
+	'rgb(239,138,98)',
+	6,
+	'rgb(178,24,43)'
+	],
+	'circle-stroke-color': 'white',
+	'circle-stroke-width': 1,
+	// Transition from heatmap to circle layer by zoom level
+	'circle-opacity': [
+	'interpolate',
+	['linear'],
+	['zoom'],
+	7,
+	0,
+	8,
+	1
+	]
 	}
-});
-	// // L.geoJSON(myGeoJSON["features"]).addTo(mymap);
-	// var maxCount = -1;
-
-	// var soccerPoints = [];
-	// myGeoJSON["features"].forEach(function(feature){
-	// 	console.log("feature: " + feature);
-	// 	var cords = feature["geometry"]["coordinates"];
-	// 	var count = feature["properties"]["count"];
-	// 	var latLng = new L.latLng(cords[0], cords[1], count);
-	// 	console.log(latLng);
-	// 	if (count > maxCount) {
-	// 		maxCount = count;
-	// 	}
-	// 	soccerPoints.push(latLng);
-	// });
-	// console.log("soccerPoints: " + soccerPoints);
-
-	// console.log("maxCount: " + maxCount);
-
-	// var heat = L.heatLayer(soccerPoints, {
- //            radius: 20,
- //            blur: 15, 
- //            maxZoom: 17,
- //            max: maxCount,
- //      }).addTo(mymap);
-
-	//  heat.redraw();
-
+	}
+);
+	
 }
 
 
