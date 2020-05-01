97c722e75eaa8cc83eb7d5a23320fd785c24f7d7
EmmetMack <emack@andrew.cmu.edu>
2020-05-01 11:49:37 -0400

    Trying to add graph of probabilities in

--- homework_8/main.js
+++ homework_8/main.js
@@ -751,6 +751,35 @@ function createPlayerForLeague(teams) {
 	    .setHTML('<b>Country: </b>' + " " + e.features[0].properties.country + '<b> Count: </b>' + " " + e.features[0].properties.count)
 	    .addTo(map);
 	});
+
+	var ctx = document.getElementById('myChart').getContext('2d');
+
+	var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: Object.keys(countryProbs),
+        datasets: [{
+            label: 'Probability Player is from Country',
+            data: Object.values(countryProbs),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }});
 	
 }
 
