36eab8ae4fb1af2aee1d1d48145f205dc1e519b0
EmmetMack <emack@andrew.cmu.edu>
2020-05-02 16:30:03 -0400

    Randomly generating colors for the data

--- homework_8/main.js
+++ homework_8/main.js
@@ -755,6 +755,15 @@ function createPlayerForLeague(teams) {
 	console.log(countryProbs);
 	console.log("countryProbs keys: " + Object.keys(countryProbs));
 
+
+	if (typeof myChart !== "undefined") {
+		myChart.destroy();
+	}
+	var randomColorGenerator = function () { 
+
+    	return '#' + (Math.random().toString(16) + '0000000').slice(2, 8); 
+	};
+
 	var ctx = document.getElementById('myChart').getContext('2d');
 
 	
@@ -765,23 +774,7 @@ function createPlayerForLeague(teams) {
         datasets: [{
             label: 'Probability Player is from Country',
             data: Object.values(countryProbs),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            fillColor: randomColorGenerator()
         }]
     }});
 	
