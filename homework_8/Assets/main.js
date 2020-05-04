6eaf7839c89140671b0bd36bb95a62511beae41d
EmmetMack <emack@andrew.cmu.edu>
2020-05-03 22:14:19 -0400

    Trying to fix chart

--- homework_8/Assets/main.js
+++ homework_8/Assets/main.js
@@ -738,22 +738,39 @@ function createPlayerForLeague(teams) {
 
 	//creates graph that shows the probability of a player being from that country for that league
 	if (typeof(myChart) !== "undefined") {
-		myChart.destroy();
-	}
-	var ctx = document.getElementById('myChart').getContext('2d');
+		removeData(myChart);
+		addData(myChart,Object.keys(countryProbs), Object.values(countryProbs );
+	} else {
+		var ctx = document.getElementById('myChart').getContext('2d');
 
 	
-	var myChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: Object.keys(countryProbs),
-        datasets: [{
-            label: 'Probability Player is from Country',
-            data: Object.values(countryProbs),
-         	backgroundColor: "#3E9D02"
-        }]
-    }});
+		var myChart = new Chart(ctx, {
+	    type: 'horizontalBar',
+	    data: {
+	        labels: Object.keys(countryProbs),
+	        datasets: [{
+	            label: 'Probability Player is from Country',
+	            data: Object.values(countryProbs),
+	         	backgroundColor: "#3E9D02"
+	        }]
+	    }});
+	}
 	
+}
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
 }
 
 var countryProbs = {}; //probability that a player is from the country for the league
